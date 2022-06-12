f756ab4c2a027f4805e70e92671a3892aae3622f
yuria imaizumi <xbyir2578@outlook.jp>
2022-06-12 14:00:49 +0000

    follow, unfollow

---
+++ app/Http/Controllers/UserFollowController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserFollowController extends Controller
+{
+    public function store($id)
+    {
+        \Auth::user()->follow($id);
+        return back();
+    }
+
+    /**
+     * ユーザをアンフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        \Auth::user()->unfollow($id);
+        return back();
+    }
+}
